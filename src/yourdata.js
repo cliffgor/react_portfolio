45fb692e02216c45281ce99110a84c3ba761edd6
cliffgor <clierozigy@gmail.com>
2020-04-09 19:01:14 +0300

    react website portfolio

---
+++ src/yourdata.js
@@ -0,0 +1,84 @@
+export default {
+  //(Please Do Not Remove The comma(,) after every variable)
+  //Change The Website Template
+  name: "Cliff Gor.",
+  headerTagline: [
+    //Line 1 For Header
+    "Hi 👋 You found me! I am Cliff Gor.",
+    //Line 2 For Header
+    "Software Engineer",
+    //Line 3 For Header
+    "from Kenya",
+  ],
+  //Contact Email
+  contactEmail: "cliffordgor@hotmail.com",
+  // Add Your About Text Here
+  abouttext:
+    "Dynamic and passionate Software Developer with experience in designing and developing user-centric and robust systems and applications. Strategic and creative thinker, with the ability to articulate and present technical matters to all audiences. Owned software development strategy from concept to implementation and user experience. Fluent in English and Kiswahili.",
+  aboutImage:
+    "https://images.unsplash.com/photo-1521587765099-8835e7201186?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=500&q=60",
+  //Change This To Hide The Image of About Section (True Or False)
+  ShowAboutImage: false, //true or false (Change Here)
+  // Change Projects Here
+  projects: [
+    {
+      id: 1, //DO NOT CHANGE THIS (Please)😅
+      title: "Wandako", //Project Title - Add Your Project Title Here
+      service: "Web Design, Ui, UX", // Add Your Service Type Here
+      //Project Image - Add Your Project Image Here
+      imageSrc:
+        "https://res.cloudinary.com/dvteezj8i/image/upload/v1586444825/Home_Page_rwatzw.png",
+      //Project URL - Add Your Project Url Here
+      url:
+        "https://xd.adobe.com/view/b51f634e-26cc-475c-5c90-620294782e8d-c32e/",
+    },
+    {
+      id: 2, //DO NOT CHANGE THIS (Please)😅
+      title: "Famolit",
+      service: "Website Development",
+      imageSrc:
+        "https://res.cloudinary.com/dvteezj8i/image/upload/v1586445424/famolit_page_ovkuol.png",
+      url: "https://www.famolit.org/",
+    },
+    {
+      id: 3, //DO NOT CHANGE THIS (Please)😅
+      title: "USSD",
+      service: "Web App",
+      imageSrc:
+        "https://res.cloudinary.com/dvteezj8i/image/upload/v1586445954/Capture_giyzjv.png",
+      url: "https://github.com/cliffgor/Ussd",
+    },
+    // {
+    //   id: 4, //DO NOT CHANGE THIS (Please)😅
+    //   title: "Project Four",
+    //   service: "Branding",
+    //   imageSrc:
+    //     "https://images.unsplash.com/photo-1558452919-08ae4aea8e29?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=500&q=60",
+    //   url: "http://chetanverma.com/",
+    // },
+
+    /*
+
+                    If You Want To Add More Project just Copy and Paste This At The End (Update the id Respectively)
+                ,{
+                id: 5,
+                title: 'Project Five',
+                service: 'Something Amazing',
+                imageSrc: "",
+                url: ''
+            }
+                */
+  ],
+  social: [
+    // Add Or Remove The Link Accordingly
+    { name: "Github", url: "https://github.com/cliffgor" },
+    {
+      name: "Behance",
+      url: "https://www.behance.net/cliffgor",
+    },
+    {
+      name: "Instagram",
+      url: "https://www.instagram.com/",
+    },
+  ],
+};
